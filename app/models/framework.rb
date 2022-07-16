cd401d7f0c330d4b7c069511b0b0d530e84759ae
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-07-16 12:54:11 -0500

    Adding associations.

--- app/models/framework.rb
+++ app/models/framework.rb
@@ -5,5 +5,8 @@ class Framework < ApplicationRecord
     has_rich_text :description
     has_many :criteria, dependent: :destroy
 
+    has_many :template_frameworks, dependent: :destroy
+    has_many :templates, through: :template_frameworks, dependent: :destroy
+
     after_create_commit {broadcast_prepend_to "frameworks"}
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,2 +1,10 @@
 class Template < ApplicationRecord
+
+    validates :name, presence: true
+    validates :description, presence: true
+
+    has_rich_text :description
+
+    has_many :template_frameworks, dependent: :destroy
+    has_many :frameworks, through: :template_frameworks, dependent: :destroy
 end
